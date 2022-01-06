d548c7aa7e29df0906ffeda0ca39871e207811c1
Eddie Rubeiz <erubeiz@sciencehistory.org>
2022-01-06 15:11:27 -0500

    fixing typo

--- export_archivesspace_xml/lib/index_page.rb
+++ export_archivesspace_xml/lib/index_page.rb
@@ -1,7 +1,7 @@
 module ExportArchivesspaceXml
   def index_page_html(collection_ids)
     list = collection_ids.map do |id|
-      "<li><a href =\"http://ENV["AWS_BUCKET"]/#{id}.ead.xml\" >#{id}</a></li>"
+      "<li><a href =\"http://#{ENV["AWS_BUCKET"]}/#{id}.ead.xml\" >#{id}</a></li>"
     end.join
 
     index = "<html xmlns=\"http://www.w3.org/1999/xhtml\" >
