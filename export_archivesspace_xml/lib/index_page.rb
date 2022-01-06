71209298db4d1c545077a3e109f8c4ba837eface
Eddie Rubeiz <erubeiz@sciencehistory.org>
2022-01-06 14:54:04 -0500

    use bucket name from ENV

--- export_archivesspace_xml/lib/index_page.rb
+++ export_archivesspace_xml/lib/index_page.rb
@@ -1,8 +1,9 @@
 module ExportArchivesspaceXml
   def index_page_html(collection_ids)
     list = collection_ids.map do |id|
-      "<li><a href =\"https://science-history-institute-archives-ead.s3.amazonaws.com/#{id}.ead.xml\" >#{id}</a></li>"
+      "<li><a href =\"http://ENV["AWS_BUCKET"]/#{id}.ead.xml\" >#{id}</a></li>"
     end.join
+
     index = "<html xmlns=\"http://www.w3.org/1999/xhtml\" >
     <head>
         <title>Science History Institute EADs</title>
@@ -14,6 +15,7 @@ module ExportArchivesspaceXml
       </ul>
     </body>
     </html>"
+    
     index
   end
 end
