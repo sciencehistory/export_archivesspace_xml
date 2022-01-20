94b31651d5323409c89dcf1e0464877c819174ae
Eddie Rubeiz <erubeiz@sciencehistory.org>
2022-01-20 09:11:49 -0500

    adding 3 new terraform resources to manage the IAM user and its policy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -39,6 +39,66 @@ resource "aws_s3_bucket_ownership_controls" "ead" {
 resource "aws_s3_bucket_policy" "ead" {
     bucket = aws_s3_bucket.ead.id
     policy = templatefile("templates/s3_public_read_policy.tftpl", { bucket_name: aws_s3_bucket.ead.id })
-
 }
 
+
+# terraform import aws_iam_user.export_archivesspace_xml_user export_archivesspace_xml
+resource "aws_iam_user" "export_archivesspace_xml_user" {
+    force_destroy = false
+    name = "export_archivesspace_xml"
+    tags          = {
+        "associated_heroku_site" = "https://dashboard.heroku.com/apps/export-archivesspace-xml"
+        "associated_s3_bucket"   = "science-history-institute-archives-ead"
+        "description"            = "This is used to export ArchivesSpace EADs to the s3 bucket."
+    }
+    tags_all          = {
+        "associated_heroku_site" = "https://dashboard.heroku.com/apps/export-archivesspace-xml"
+        "associated_s3_bucket"   = "science-history-institute-archives-ead"
+        "description"            = "This is used to export ArchivesSpace EADs to the s3 bucket."
+    }
+}
+
+#  arn:aws:iam::335460257737:policy/export_archivesspace_xml_policy 
+
+#  aws_iam_policy.export_archivesspace_xml_policy
+
+# terraform import aws_iam_policy.export_archivesspace_xml_policy arn:aws:iam::335460257737:policy/export_archivesspace_xml_policy 
+
+
+resource "aws_iam_policy" "export_archivesspace_xml_policy" {
+  name        = "export_archivesspace_xml_policy"
+  path        = "/"
+  description = "Eddie created this policy in January 2020. The policy allows IAM user export_archivesspace_xml to export EAD files from ArchivesSpace, via the API, to s3 bucket science-history-institute-archives-ead ." 
+
+
+  tags        = {
+          "associated_project" = "export-archivesspace-xml"
+          "description"        = "Allows user export_archivesspace_xml read - write - and list privileges on bucket science-history-institute-archives-ead ."
+  }
+  tags_all    = {
+            "associated_project" = "export-archivesspace-xml"
+            "description"        = "Allows user export_archivesspace_xml read - write - and list privileges on bucket science-history-institute-archives-ead ."
+    }
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject",
+                "s3:PutObjectTagging"
+            ],
+            "Resource": [
+                "arn:aws:s3:::ead.sciencehistory.org/*",
+                "arn:aws:s3:::ead.sciencehistory.org"
+            ]
+        }
+    ]
+})
+}
+
+resource "aws_iam_user_policy_attachment" "export-archivesspace-xml_user_policy_attachment" {
+   user       = aws_iam_user.export_archivesspace_xml_user.name
+   policy_arn = aws_iam_policy.export_archivesspace_xml_policy.arn
+}
